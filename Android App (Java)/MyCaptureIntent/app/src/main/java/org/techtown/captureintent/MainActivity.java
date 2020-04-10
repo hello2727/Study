6d8e1749f6460ee211c556f97ed4952745d6d1a0
hello2727 <jihye7369@gmail.com>
2020-04-10 22:30:30 +0900

    멀티미디어 다루기(카메라로 사진찍어 저장:라이브러리, manifest permission설정 필요)

---
+++ Android App (Java)/MyCaptureIntent/app/src/main/java/org/techtown/captureintent/MainActivity.java
@@ -0,0 +1,101 @@
+package org.techtown.captureintent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
+    ImageView imageView;
+    File file;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture(); //사진찍기
+            }
+        });
+
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
+    }
+
+    public void takePicture() {
+        //용량이 크므로 파일 처리
+        if (file == null) {
+            file = createFile();
+        }
+
+        Uri fileUri = FileProvider.getUriForFile(this, "org.techtown.captureintent.fileprovider", file);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 101);
+        }
+    }
+
+    //파일 만들기
+    public File createFile() {
+        String filename = "capture.jpg";
+
+        File storageDir = Environment.getExternalStorageDirectory();
+        File outFile = new File(storageDir, filename);
+
+        return outFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101 && resultCode == RESULT_OK) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 6;
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options); //이미지 샘플링
+
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    @Override
+    public void onDenied(int requestCode, String[] permissions) {
+        Toast.makeText(this, "permissions denied : " + permissions.length,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGranted(int requestCode, String[] permissions) {
+        Toast.makeText(this, "permissions granted : " + permissions.length, Toast.LENGTH_LONG).show();
+    }
+
+}
