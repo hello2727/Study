047e8553d2d5bdf840f64082bffb36da660530f8
hello2727 <jihye7369@gmail.com>
2020-04-13 18:23:17 +0900

    DB에서 데이터 읽어와 화면에 출력하여 웹페이지 구성하기

--- PHP & MySQL/create.php
+++ PHP & MySQL/create.php
@@ -1,3 +1,33 @@
+<?php
+//<?=:echo랑 같은 의미
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  'dnjseldkej347#W',
+  'opentutorials');
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)) {
+  //<li><a href=\"index.php?id=6\">MySQL</a></li>
+  //데이터베이스에 있는 순서대로 링크 목록으로 만들기
+  $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+
+$article = array(
+  'title'=>'Welcome',
+  'description'=>'Hello, web'
+);
+if(isset($_GET['id'])) {
+  $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  $article['title'] = $row['title'];
+  $article['description'] = $row['description'];
+}
+
+?>
 <!doctype html>
 <html>
   <head>
@@ -5,9 +35,9 @@
     <title>WEB</title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
-      <li>HTML</li>
+      <?=$list?>
     </ol>
     <form action="process_create.php" method="POST">
       <p><input type="text" name="title" placeholder="title"></p>
