ddbffdc1009ad65b1af921f8c3b3afb3e7d4a2ee
hello2727 <jihye7369@gmail.com>
2020-06-16 11:46:09 +0900

    JSON Parsing

---
+++ Android App (Java)/ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,48 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+
+    private double mMagnitude;
+    private String mLocation;
+    private long mTimeInMilliseconds;
+    private String mUrl;
+
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url){
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public double getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public void setmMagnitude(double mMagnitude) {
+        this.mMagnitude = mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public void setmLocation(String mLocation) {
+        this.mLocation = mLocation;
+    }
+
+    public long getmTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public void setmTimeInMilliseconds(long mTimeInMilliseconds) {
+        this.mTimeInMilliseconds = mTimeInMilliseconds;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
